520fb0021394e2bea3b54b2963561c5ba06d0a42
Мельников Николай <fireminergoodplay@gmail.com>
2023-01-10 18:20:02 +0300

    add HTML, posts, css

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    template = 'posts/index.html'
+    description = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'description': description,
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'Your slug: {slug}')
+def group_list(request):
+    template = 'posts/group_list.html'
+    description = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'description': description,
+    }
+    return render(request, template, context)
